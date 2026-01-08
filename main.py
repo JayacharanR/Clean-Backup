5edfc92ac1bdf5ad713213b140cb7b7ab556ff7c
JayacharanR <jayacharan.2023@vitstudent.ac.in>
2026-01-08 23:26:08 +0530

    Changed inital user input to infinite loop for better usability

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from src.organiser import organise_files
 
 def main():
-    print("Clean-Backup: Photo & Video Organizer")
-    print("-------------------------------------")
-    source = input("Enter source folder path: ")
-    destination = input("Enter destination folder path: ")
-    
-    if source and destination:
-        organise_files(source, destination)
-        print("\nOrganization complete! Check logs for details.")
-    else:
-        print("Source and Destination paths are required.")
+    source=None
+    destination=None
+    while (not source and not destination):
+        print("Clean-Backup: Photo & Video Organizer")
+        print("-------------------------------------")
+        source = input("Enter source folder path: ")
+        destination = input("Enter destination folder path: ")
+        if source and destination:
+            organise_files(source, destination)
+            print("\nOrganization complete! Check logs for details.")
+        else:
+            print("Source and Destination paths are required.")
 
 if __name__ == "__main__":
     main()
