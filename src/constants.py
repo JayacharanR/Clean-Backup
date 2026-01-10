290199698fdbf818715976bd4ae30f8209027067
JayacharanR <jayacharan.2023@vitstudent.ac.in>
2026-01-10 13:37:01 +0530

    Added .raf format

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,3 @@
 # Constants for file extensions
-IMAGE_EXTENSIONS = {'.jpg', '.jpeg', '.png', '.heic', '.bmp', '.tiff', '.gif'}
+IMAGE_EXTENSIONS = {'.jpg', '.jpeg', '.png', '.heic', '.bmp', '.tiff', '.gif','.raf'}
 VIDEO_EXTENSIONS = {'.mp4', '.mov', '.avi', '.mkv', '.wmv', '.flv', '.webm'}
